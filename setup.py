7922e71786bb862afeff370385403c132fd8905b
Julian M. Kleber <jkdadww@icloud.com>
2021-12-06 18:54:57 +0100

    fixed some setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(
-    name="continious-template",
+    name="continuous-template",
     version="0.1",
     description="A package supporting rapid python development with GitHub CI",
-    url="https://github.com/MQSchleich/chembee",
+    url="https://github.com/MQSchleich/continuous-template",
     author="Peter Parker",
     author_email="julian.m.kleber@gmail.com",
     license="MIT",
